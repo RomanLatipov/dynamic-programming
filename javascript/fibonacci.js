f03864da8992d236c9bb6858443a1b95659726f5
RomanLatipov <romanlatipov@gmail.com>
2024-07-24 21:09:23 -0400

    complete dynamic-programing lab

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,11 +1,14 @@
+const fibNums = [0,1];
 function fibonacci(num) {
-// type your code here
+    if(fibNums[num] !== undefined) return fibNums[num];
+    fibNums.push(fibonacci(num-1) + fibonacci(num-2));
+    return fibNums[num];
 }
 
 if (require.main === module) {
 // add your own tests in here
 console.log("Expecting: 0");
-console.log("=>", fibonacci(0));
+console.log("=>", fibonacci(2));
 
 console.log("");
 
